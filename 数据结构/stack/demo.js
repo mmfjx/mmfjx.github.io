c0766218a07a0a18a255b79f3fc2726bc72af2da
maominfang <maominfang@meituan.com>
2019-11-06 23:13:32 +0800

    数据结构

---
+++ 数据结构/stack/demo.js
@@ -0,0 +1,8 @@
+import Stack from './stack.js'
+
+let oneStack = new Stack();
+oneStack.push(1);
+oneStack.push(2);
+// oneStack.pop();
+console.log(oneStack.size)
+console.log(oneStack)
---
+++ 数据结构/stack/stack.js
@@ -0,0 +1,25 @@
+export default class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(val) {
+        this.items.push(val);
+    }
+
+    pop(){
+        this.items.pop();
+    }
+
+    get isEmpty() {
+        return !this.items.length;
+    }
+
+    get size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items= [];
+    }
+}
