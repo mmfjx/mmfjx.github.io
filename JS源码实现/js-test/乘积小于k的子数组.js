0fc9786c83e932de29e9fcb25ba298ab851bd7ea
mmfjx <elsiaperson@163.com>
2024-03-11 15:40:46 +0800

    update

---
+++ JS源码实现/js-test/乘积小于k的子数组.js
@@ -0,0 +1,31 @@
+// // 输入一个由正整数组成的数组和一个正整数k， 请问数组中有多少个数字乘积小于k的连续子数组？
+const arr = [10, 5, 2, 6]
+const k = 100
+// 有8个子数组，[10]、[5]、[2]、[6]、[10,5]、[5,2]、[2,6]、[5,2,6]
+
+// 思路： 双指针方式
+function fn1(arr, k) {
+    let p1 = 0
+    let p2 = 0
+    const res = []
+
+    //
+    for (let i = 0; i < arr.length; i++) {
+        p1 = i
+        p2 = i
+        while (p1 <= p2 && p2 < arr.length) {
+            const product = arr
+                .slice(p1, p2 + 1)
+                .reduce((acc, a) => (acc *= a), 1)
+            if (product < k) {
+                res.push(arr.slice(p1, p2 + 1))
+                p2++
+            } else {
+                break
+            }
+        }
+    }
+
+    return res
+}
+console.log(fn1(arr, k))
---
+++ JS源码实现/js-test/和大于或等于k的最短子数组.js
@@ -0,0 +1,31 @@
+// 输入一个正整数组成的数组和一个正整数k, 请问数组中和大于或等于k的连续子数组的最短长度是多少？若是不存在，则返回0
+const arr = [5, 1, 4, 3]
+const k = 7
+
+// 思路： 双指针的方式，因求的是连续子数组的最短长度，因此不需要排序
+function fn1(arr, k) {
+    let len = 0
+    for (let i = 0; i < arr.length; i++) {
+        let p1 = i
+        let p2 = i
+        while (p2 < arr.length && p1 <= p2) {
+            const sum = arr
+                .slice(p1, p2 + 1)
+                .reduce((acc, curr) => acc + curr, 0)
+            if (sum >= k) {
+                const temp = p2 - p1 + 1
+                if (!len) {
+                    len = temp
+                } else {
+                    len = Math.min(temp, len)
+                }
+                break
+            } else if (sum < k) {
+                p2++
+            }
+        }
+    }
+
+    return len
+}
+console.log(fn1(arr, k))
